6af6c177d7df47c321b2977e8631ace88c9b05b3
Promiko <itshag63@yandex.ru>
2022-04-14 20:09:07 +0400

    My program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp86 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Привет! Это моя первая программа на C#!");
+        }
+    }
+}
